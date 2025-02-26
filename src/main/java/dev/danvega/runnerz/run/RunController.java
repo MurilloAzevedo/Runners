dbd6609b29cde0d145b3e0b9a0471bbcb6f67c29
Murillo José de Azevedo <murilloazevedo2@gmail.com>
2025-02-25 21:31:38 -0300

    criando o create e utilizando o Postman

--- src/main/java/dev/danvega/runnerz/run/RunController.java
+++ src/main/java/dev/danvega/runnerz/run/RunController.java
@@ -1,12 +1,12 @@
 package dev.danvega.runnerz.run;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/runs")
@@ -25,6 +25,17 @@ public RunController(RunRepository runRepository) {
 
     @GetMapping("/{id}")
     Run findById(@PathVariable Integer id){
-        return runRepository.findById(id);
+
+        Optional<Run> run = runRepository.findById(id);
+        if (run.isEmpty()){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Run not found");
+        }
+        return run.get();
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("")
+    void create(@RequestBody Run run){
+        runRepository.create(run);
     }
 }
