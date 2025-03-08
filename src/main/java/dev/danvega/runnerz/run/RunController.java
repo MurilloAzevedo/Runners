35106e2a124b3baa4e054713659a9b1b7561b954
Murillo José de Azevedo <murilloazevedo2@gmail.com>
2025-03-08 12:19:18 -0300

    Finalizando o CRUD

--- src/main/java/dev/danvega/runnerz/run/RunController.java
+++ src/main/java/dev/danvega/runnerz/run/RunController.java
@@ -1,5 +1,6 @@
 package dev.danvega.runnerz.run;
 
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
@@ -28,14 +29,27 @@ public RunController(RunRepository runRepository) {
 
         Optional<Run> run = runRepository.findById(id);
         if (run.isEmpty()){
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Run not found");
+            throw new RunNotFoundException(id);
         }
         return run.get();
     }
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("")
-    void create(@RequestBody Run run){
-        runRepository.create(run);
+    void create(@Valid @RequestBody Run run){
+
+    runRepository.create(run);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/{id}")
+    void update(@Valid @RequestBody Run run, @PathVariable Integer id){
+        runRepository.update(run, id);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    void delete(@PathVariable Integer id){
+        runRepository.delete(id);
     }
 }
--- src/main/java/dev/danvega/runnerz/run/RunRepository.java
+++ src/main/java/dev/danvega/runnerz/run/RunRepository.java
@@ -27,6 +27,17 @@ public class RunRepository {
         runs.add(run);
     }
 
+    void update(Run run, Integer id) {
+        Optional<Run> existingRun = findById(id);
+        if (existingRun.isPresent()) {
+            runs.set(runs.indexOf(existingRun.get()), run);
+        }
+    }
+
+    void delete(Integer id) {
+        runs.removeIf(run -> run.id().equals(id));
+    }
+
     @PostConstruct
     private void init() {
         runs.add(new Run(1, "Morning Run", LocalDateTime.now(), LocalDateTime.now().plusMinutes(60), 5, Location.INDOOR));
